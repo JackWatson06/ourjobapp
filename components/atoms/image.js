1e6d319c190e3bd3282337ffdd2fe4052103c9d3
Jack Watson <watson.jack.p@gmail.com>
2021-11-07 18:03:55 -0500

    Social Media Colors and Graphic Updates

--- components/atoms/image.js
+++ components/atoms/image.js
@@ -9,7 +9,7 @@ export default function Image(props) {
   const { ...rest } = props;
   return (
     <div className={style.ImageContainer}>
-      <NextImage className={style.BaseImage} {...rest} layout="fill" />
+      <NextImage className={style.BaseImage} {...rest} layout="fill" priority={true} />
     </div>
   );
 };
--- components/molecules/footer-socials.js
+++ components/molecules/footer-socials.js
@@ -9,22 +9,16 @@ import SocialLinks from '@lib/utilities/SocialLinks'
 export default function FooterSocials()
 {
     return<div className={styles.IconContainer}>
-        <svg width="0" height="0" viewBox="0 0 0 0" xmlns="http://www.w3.org/2000/svg">
-            <linearGradient x1="0" y1="0" x2="100%" y2="100%" id="icon-gradient">
-                <stop className={styles.FirstStop} offset="0"/>
-                <stop className={styles.SecondStop} offset="100%"/>
-            </linearGradient>
-        </svg>
-        <Link href={SocialLinks.instagram}>
+        <Link href={SocialLinks.instagram} passHref={true}>
             <FontAwesomeIcon className={styles.Icon} icon={ faInstagram } />
         </Link>       
-        <Link href={SocialLinks.tiktok}>
+        <Link href={SocialLinks.tiktok} passHref={true}>
             <FontAwesomeIcon className={styles.Icon} icon={ faTiktok } />
         </Link>
-        <Link href={SocialLinks.twitter}>
+        <Link href={SocialLinks.twitter} passHref={true}>
             <FontAwesomeIcon className={styles.Icon} icon={ faTwitter } />
         </Link>
-        <Link href={SocialLinks.facebook}>
+        <Link href={SocialLinks.facebook} passHref={true}>
             <FontAwesomeIcon className={styles.Icon} icon={ faFacebook } />
         </Link>
     </div>
