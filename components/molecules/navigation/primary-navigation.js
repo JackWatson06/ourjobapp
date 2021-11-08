fdc7b120fcd442c14d9c2c0a2a070e7d43fbece9
Jack Watson <watson.jack.p@gmail.com>
2021-11-07 20:19:14 -0500

    Logo

--- components/molecules/navigation/primary-navigation.js
+++ components/molecules/navigation/primary-navigation.js
@@ -9,12 +9,6 @@ import SocialLinks from '@lib/utilities/SocialLinks'
 export default function PrimaryNavigation()
 {
     return <div className={styles.IconContainer}>
-        <svg width="0" height="0" viewBox="0 0 0 0" xmlns="http://www.w3.org/2000/svg">
-            <linearGradient x1="0" y1="0" x2="100%" y2="100%" id="icon-gradient">
-                <stop className={styles.FirstStop} offset="0"/>
-                <stop className={styles.SecondStop} offset="100%"/>
-            </linearGradient>
-        </svg>
         <Link href={SocialLinks.instagram}>
             <FontAwesomeIcon className={styles.Icon} icon={ faInstagram } />
         </Link>       
